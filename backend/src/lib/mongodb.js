735aa824805720a6105b81adbaa4262bcbf7a557
Pankaj Yadav <yadavpankaj.tech@gmail.com>
2025-03-16 17:36:41 +0000

    fix the error in the database connection

--- backend/src/lib/mongodb.js
+++ backend/src/lib/mongodb.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/resume-builder';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://172.17.96.1:27017/resume-builder';
 
 if (!MONGODB_URI) {
   throw new Error(
